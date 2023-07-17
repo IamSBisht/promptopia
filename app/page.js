de73bd200f1a42c30cfbdec5af8b5c6b440b0231
iamsbisht <sukhdeep.bisht@gmail.com>
2023-07-17 23:54:16 +0530

    New prompt functionality

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@ import Feed from '@/components/Feed'
 
 const Home = () => {
    return (
-      <>
       <section className='w-full flex-center flex-col'>
          <h1 className='head_text text-center'>Discover and Share
             <br className="max-md:hidden" />
@@ -14,7 +13,7 @@ const Home = () => {
          </p>
          <Feed />
       </section>
-      </> )
+   )
 }
 
 export default Home
--- components/Feed.js
+++ components/Feed.js
@@ -1,8 +1,20 @@
-import React from 'react'
+'use client'
+import { useState, useEffect } from "react"
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState('')
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input type="text" placeholder="Search for a tag or username"
+          value={searchText}
+          onChange={(e) => { setSearchText(e.target.value) }}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+    </section>
   )
 }
 
