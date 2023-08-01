26fae58ef885b863ce480d500dcb494c8a249987
mgupta@mind-roots.com <sukhdeep.bisht@gmail.com>
2023-08-01 18:29:58 +0530

    Changes

--- app/profile/page.js
+++ app/profile/page.js
@@ -7,6 +7,7 @@ import { useRouter } from "next/navigation"
 import Profile from "@/components/Profile"
 
 const UserProfile = () => {
+    const router = useRouter();
     const { data: session } = useSession();
     const [posts, setPosts] = useState([]);
 
@@ -14,14 +15,29 @@ const UserProfile = () => {
         const fetchPosts = async () => {
             const response = await fetch(`/api/users/${session?.user.id}/posts`)
             const data = await response.json();
-            console.log({data})
             setPosts(data);
         }
         if (session?.user.id) fetchPosts()
-    },[])
+    }, [])
 
-    const handleEdit = () => { }
-    const handleDelete = async () => { }
+    const handleEdit = (post) => {
+        router.push(`/update-prompt?id=${post._id}`)
+    }
+    const handleDelete = async (post) => {
+        const hasConfirmed = confirm("Are you sure you want to delete this prompt?")
+        if (!hasConfirmed) return
+        if (hasConfirmed) {
+            try {
+                await fetch(`/api/prompt/${post._id.toString()}`, {
+                    method: "DELETE"
+                })
+                const filterdPosts = myPosts.filter(p => p._id !== post._id)
+                setPosts(filterdPosts);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
 
     return (
         <Profile
